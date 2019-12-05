468648ea434b0a53afe7522964963b924c0e4569
chama <rk12381995@gmail.com>
2019-12-05 23:46:17 +0900

    ABC145A

--- ABC085C_Otoshidama.py
+++ ABC085C_Otoshidama.py
@@ -5,7 +5,7 @@ for a in range(n + 1):
     for b in range(n + 1):
         if 10000 * a + 5000 * b + (n - a - b) * 1000 == y and (n-a-b) >= 0:
             ans = '{} {} {}'.format(a, b, (n-a-b))
-            break
+            break   #ここでprintしてexit()でプログラム終了でも可
     if ans != '-1 -1 -1':
         break
 
